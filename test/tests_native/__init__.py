0080ec94a8826e65c4a7a4c01c878aa140ef640a
Some One <some.one@mail.com>
2026-09-23 13:45:29 +0300

    delete test embed git info

--- test/tests_native/__init__.py
+++ test/tests_native/__init__.py
@@ -5,4 +5,3 @@ from .test_wallet_manager_warnings import *
 from .test_change_classification import *
 from .test_transaction_confirmation import *
 from .test_change_security import *
-from .test_embed_git_info import *
